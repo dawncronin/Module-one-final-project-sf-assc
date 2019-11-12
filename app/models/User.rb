f0bac92d9d2aad4372ef96c58a08e34242db9cb9
github username <github email address>
2019-11-12 10:25:40 -0800

    made model and migration files for user and user_event

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,7 @@
-class User
+class User < ActiveRecord::Base
+    has_many :user_events
+    has_many :events, through: :user_events
+    has_many :preferences
+    has_many :categories, through: :preferences
 
 end
--- db/migrate/001_Create_Users.rb
+++ db/migrate/001_Create_Users.rb
@@ -1,1 +1,8 @@
-puts "0"
+class CreateUsers < ActiveRecord::Migration[5.2]
+    def change
+        create_table :users do |t|
+            t.string :username
+            t.timestamps
+        end
+    end
+end
