a624bae6f24cac7a26e3228282cc7b8fb3f7d4ca
github username <github email address>
2019-11-14 10:38:34 -0800

    added cli stats

---
+++ app/cli_stats.rb
@@ -0,0 +1,8 @@
+def stats
+    puts "Please pick a statistic by number:"
+    puts "1. Most Popular Events"
+    puts "2. Most Popular Preferences by user"
+    puts "3. Number of friends"
+    puts "4. Most Popular Categories by friend's events"
+    puts "5. Return to Main Menu"
+end
